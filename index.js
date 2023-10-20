1b943ecf1cad2fe7644b7b1d0d635be95d6d5138
MARIA DOS NAVEGANTES OLIVEIRA DA SILVA <MARIA0762@DIGITALCOLLEGE.LOCAL>
2023-10-20 19:37:49 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Leitura dos valores A e B (você pode inserir os valores desejados)
+var A = 5;
+var B = 5;
+
+// Verifica se A e B são iguais
+if (A === B) {
+    // Se forem iguais, some os valores
+    var C = A + B;
+} else {
+    // Se forem diferentes, multiplique A por B
+    var C = A * B;
+}
+
+// Mostra o resultado na tela
+console.log("O resultado é: " + C);
